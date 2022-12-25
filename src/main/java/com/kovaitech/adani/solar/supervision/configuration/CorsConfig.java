171b1019bcfd3a95d22489a2fc710de0143615f2
Karthick Selvaraj <karthickselvaraj@Karthicks-MacBook-Pro.local>
2022-12-25 12:21:08 +0530

    Adding cross filter

---
+++ src/main/java/com/kovaitech/adani/solar/supervision/configuration/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.kovaitech.adani.solar.supervision.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /**
+     * Overriding the CORS configuration to exposed required header for ussd to work
+     *
+     * @param registry CorsRegistry
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(4800);
+    }
+}
